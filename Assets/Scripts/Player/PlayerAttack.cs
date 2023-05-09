43dded198f7295233c6063b7ec99e7b9a3193544
lufor <1056252297@163.com>
2023-05-09 21:39:46 +0800

    finish attack function

--- Assets/Scripts/Player/PlayerAttack.cs
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    public PlayerVariable playerVariable;
+    public Rigidbody2D ThisRigbody;
+    public GameObject BulletPrefeb;
+    public PlayerJump playerJump;
+    public bool AttackReady = true;
+    public float CoolDownCount = 4f;
+    public float attackValue = 45f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerVariable = GetComponent<PlayerVariable>();
+        ThisRigbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Attack();
+    }
+
+    void Attack()
+    {//实例化swordLight
+        Quaternion SwordRotation = Quaternion.AngleAxis(playerJump.JumpAngle, Vector3.forward);
+        if (AttackReady == false)
+        {
+            CoolDownCount -= Time.deltaTime;
+            if (CoolDownCount < 0)
+            {
+                AttackReady = true;
+                CoolDownCount = 2;
+            }
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (AttackReady == true)
+            {
+
+                GameObject instance = Instantiate(BulletPrefeb, playerJump.SelfPos, SwordRotation);
+                SwordAttack swordAttack = instance.GetComponent<SwordAttack>();
+                swordAttack.AttackValue = attackValue;
+                AttackReady = false;
+            }
+        }
+
     }
 }
