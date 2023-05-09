27368998f1492871ed9e4de64e12433d65303b01
lufor <1056252297@163.com>
2023-05-09 21:47:15 +0800

    add a Cycler to delete bullets out range

--- Assets/Scripts/Bullets/SwordAttack.cs
+++ Assets/Scripts/Bullets/SwordAttack.cs
@@ -5,15 +5,26 @@ using UnityEngine;
 public class SwordAttack : MonoBehaviour
 {
     public float AttackValue;
+    public Rigidbody2D SwordRigid;
+    public Vector2 ThisPos;
+    public float SelfFlySpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        SwordRigid = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SwordRigid.velocity = transform.right * SelfFlySpeed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("EnemyBallet"))
+        {
+            Destroy(other.gameObject);
+        }
     }
 }
