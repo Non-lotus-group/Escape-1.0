73df58dc91ad4ccdbccf6f5ea5203a7320493d64
lufor <1056252297@163.com>
2023-05-18 23:41:42 +0800

    add a item for when player land spawn some stars

--- Assets/Scripts/Collections/CollBase.cs
+++ Assets/Scripts/Collections/CollBase.cs
@@ -9,9 +9,10 @@ public abstract class CollBase
     public virtual void OnHit(PlayerVariable player, EnemyBase enemy, int stacks) { }
     public virtual void Missile(PlayerVariable player, int stacks) { }
     public virtual void OnKill() { }
-    public virtual void OnLand(PlayerVariable player, int stacks) { }
+    public virtual void OnLand(PlayerVariable player, int stacks, bool Land, Vector3 PlayerNormal, GameObject Star) { }
 }
-public class HealingItem : CollBase {
+public class HealingItem : CollBase
+{
     public override string GiveName()
     {
         return "Healing Items";
@@ -19,5 +20,31 @@ public class HealingItem : CollBase {
     public override void Update(PlayerVariable player, int stacks)
     {
         player.Health += stacks * 2;
+    }
+}
+public class JumpStars : CollBase
+{
+    public override string GiveName()
+    {
+        return "Shoot Sparks";
+    }
+    public override void OnLand(PlayerVariable player, int stacks, bool Land, Vector3 PlayerNormal, GameObject Star)
+    {
+        if (Land == true)
+        {
+            int SpawnNum = Random.Range(stacks, stacks + 3);
+            for (int i = 0; i < SpawnNum; i++)
+            {
+                GameObject NewStar = GameObject.Instantiate(Star, player.transform.position, Quaternion.identity);
+                float StarForce = Random.Range(2f, 7f);
+                Vector3 StarDir = PlayerNormal + (Random.Range(-3f, 3f) * player.transform.right);
+                NewStar.GetComponent<StarBullet>().GetComponent<Rigidbody2D>().AddForce(StarForce*StarDir, ForceMode2D.Impulse);
+                NewStar.transform.rotation = Quaternion.Euler(0, 0, Random.Range(50f,280f));
+                // add force to NewStar and angle that dir to normal <90
+                // dir force is also random range
+                // add a rotation on newstar.z 20 tp 80?
+                // NewStar's color will change or load colors on list or shader
+            }
+        }
     }
 }
--- Assets/Scripts/Player/PlayerCollections.cs
+++ Assets/Scripts/Player/PlayerCollections.cs
@@ -9,20 +9,26 @@ public class PlayerCollections : MonoBehaviour
     public PlayerVariable Pv;
     public PlayerHealth Ph;
     public float Health;
+    public GameObject StarBullet;
     // Start is called before the first frame update
     void Start()
     {
         Pv = GetComponent<PlayerVariable>();
         Health = Pv.Health;
         HealingItem heal1 = new HealingItem();
+        JumpStars StarB = new JumpStars();
         items.Add(new ItemList(heal1, heal1.GiveName(), 0));
+        items.Add(new ItemList(StarB, StarB.GiveName(), 2));
         StartCoroutine(CallItemUpdate());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        foreach (ItemList i in items)
+        {
+            i.item.OnLand(Pv, i.stacks, true, Pv.GrivityDir, StarBullet);
+        }
     }
 
     IEnumerator CallItemUpdate() {
